35d8a49909ee3468d17b12f1680476e3c2c6eb70
rebecca9105 <rebeccaramirez910@gmail.com>
2017-07-19 13:08:02 -0400

    add python shapes

---
+++ python shapes/draw_shapes.py
@@ -0,0 +1,21 @@
+from turtle import 
+import math
+
+# Name your Turtle.
+Will=(Turtle)
+
+# Set Up your screen and starting position.
+setup(500,300)
+x_pos =0
+y_pos =0
+
+### Write your code below:
+Will.forward(54)
+Will.left(90)
+Will.forward(90)
+
+
+
+
+# Close window on click.
+exitonclick(a)
